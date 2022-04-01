a5e127eb2c00fd356afd78e4dff83dc50a5236df
pauljoohyunkim <pauljoohyunkim@gmail.com>
2022-04-01 13:21:02 +0200

    Initial commit for hill

---
+++ src/symmetric/hill/hill.c
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+
+void hill(uint8_t* key, uint8_t* buffer, int configuration_num)
+{
+
+}
+
---
+++ src/symmetric/hill/hill.h
@@ -0,0 +1,8 @@
+#ifndef HILL_H
+#define HILL_H
+
+#include <stdint.h>
+// Function declaration
+void hill(uint8_t* key, uint8_t* buffer, int configuration_num);
+
+#endif
